54c2b98f2fb7775a569d0498530181a153548be3
William-Miyamoto <114786683+William-Miyamoto@users.noreply.github.com>
2025-02-10 23:57:14 -0500

    New version!

--- collapsible.js
+++ collapsible.js
@@ -5,7 +5,6 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    var pElement = content.querySelector("p");
     if (content.style.maxHeight) {
       // Collapse
       content.style.maxHeight = null; 
@@ -13,7 +12,7 @@ for (i = 0; i < coll.length; i++) {
       content.style.padding = "0px";
     } else {
       // Expand
-      content.style.maxHeight = (pElement.scrollHeight * 4) + "px"; // Dynamically set height
+      content.style.maxHeight = (content.scrollHeight * 4) + "px"; // Dynamically set height
       content.classList.add("visible");
       content.style.padding = "30px";
     }
